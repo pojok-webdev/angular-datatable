3da1b3a59f0968486e55ec0ed72fc754010421af
pujie <pw.prayitno@gmail.com>
2017-12-02 08:45:47 +0700

    add first files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UsertableComponent } from './components/usertable/usertable.component';
 
+import { MatTableModule } from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+
+import { UserService} from './services/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsertableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,8 @@
+export interface User{
+    name: string;
+    phone: string;
+    email: string;
+    company: {
+        name: string;
+    }
+}
